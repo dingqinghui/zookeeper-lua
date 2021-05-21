f037b90d5cdd388db21dd5f1d3868e31c32705a7
dqh19930227 <ding_qinghui@163.com>
2021-05-21 10:19:17 +0800

    no message

--- bin/script/main.lua
+++ bin/script/main.lua
@@ -1,97 +1,12 @@
 
 
 
-local coroutines = require("script.coroutines")
-local zkclient = require("script.zkclient")
-local zkmgr = require("script.zkmgr")
-local zookeeper = require("zookeeper")
-local define = require("script.define") 
-local WACHER_EVENT_TYPE = define.WACHER_EVENT_TYPE
 
-
-local client = nil
-
-function getnodeHandler(...)
-    print("getnodeHandler",...)
-
+a = 0
+function func()
+    debug.setupvalue( func, 1, {b = 1,print = print} )
+    print(b,a)
 end 
 
 
-
---zkmgr.init() 
-local function test1()
-     ---------------------------------------同步API测试-----------------------------------------------
-     print( "######## createnode ret:",client:createnode("/setnode","ss",3,false,false,"") )    
-     -- ######## createnode ret:        true    /setnode
-     print( "######## createnode ret:",client:createnode("/setnode/child","ss11",3,false,false,"") )
-     print( "######## setnode ret:",client:setnode("/setnode","ss2",4) )
-     --######## setnode ret:   true    /setnode
-     local ret,parentpath ,childs = client:getchilds("/setnode")
-     for k,child in pairs(childs) do
-         local childpath = string.format("%s/%s",parentpath,child)
-         print("######## childpath:",childpath)
-         print("########",client:getnode(childpath))
-     end
-     print( "######## getchilds ret:",client:getchilds("/setnode") )
-     --######## getchilds ret: true    /setnode        table: 0x1953690
-     print( "######## existnode ret:",client:existnode("/setnode") )
-     --######## existnode ret: true    /setnode
-     print( "######## getnode ret:",client:getnode("/setnode") )
-     --######## getnode ret:   true    /setnode        ss2     4
-     print( "######## delnode ret:",client:delnode("/setnode/child") )
-     --######## delnode ret:   true    /setnode/child
-     print( "######## delnode ret:",client:delnode("/setnode") )
-     ---------------------------------------同步API测试-----------------------------------------------
-end 
-
- ---------------------------------------监视点测试--------------------------------------------------------
-local function test2()
-
-    function wacherHandler(watcherType,path,...)
-        print("#############",
-            string.format("watcherType:%s path:%s",
-                    zkmgr.wacherType2String(watcherType),
-                    path
-                ),
-            ...)
-        if watcherType == WACHER_EVENT_TYPE.EventNodeCreated then 
-            
-        elseif watcherType == WACHER_EVENT_TYPE.EventNodeDeleted then
-           
-        elseif watcherType == WACHER_EVENT_TYPE.EventNodeDataChanged then
-           
-        elseif watcherType == WACHER_EVENT_TYPE.EventNodeChildrenChanged then
-            
-        else
-            return "EventWacherFail"
-        end 
-    end 
-
-    client:subscribeNode("/setnode",wacherHandler)
-    client:subscribeChildNode("/setnode",wacherHandler)
-end 
- ---------------------------------------监视点测试--------------------------------------------------------
-
-local function onConnect(isReconnenct)
-   
-   
-
-end
-
-local function onDisconnect(isExpire)
-   
-   
-
-end
-
-
-client = zkmgr.newclient("127.0.0.1:2183,127.0.0.1:2184,127.0.0.1:2185",5000,onConnect,onDisconnect)
-
-
-
-while true do
-    zkmgr.run()
-
-
-end
-
+func()
--- code/main.c
+++ code/main.c
@@ -5,12 +5,19 @@
 
 #include   <stdio.h>
 
+void* __alloc(void *ud, void *ptr, size_t osize, size_t nsize){
+    lua_State *L = ud;
+    return realloc(ptr, nsize);
+}
+
 
 int main(void)
 {
+  char buf[80];   
+  getcwd(buf,sizeof(buf));   
+  printf("current working directory: %s\n", buf);   
 
-
-  lua_State *L  = luaL_newstate();
+  lua_State *L  = lua_newstate(__alloc,L);
   luaL_openlibs(L);
 
   int retCode = luaL_dofile(L,"./script/main.lua");
@@ -19,7 +26,7 @@ int main(void)
       printf("error %s\n",lua_tostring(L,-1));
       return;
   }
-
+  
 
   lua_close(L);
   return 0;
